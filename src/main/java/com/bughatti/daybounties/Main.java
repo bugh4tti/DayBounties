667c1ce38edbec5a6f5f1c5fc7fb28f9aa38d77c
bugh4tti <quieroamiabuu@gmail.com>
2026-09-13 00:13:22 -0300

    Implement main plugin class for DayBounties

---
+++ src/main/java/com/bughatti/daybounties/Main.java
@@ -0,0 +1,63 @@
+package com.bughatti.daybounties;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+    private BountyManager bountyManager;
+    private Economy economy;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        if (!setupEconomy()) {
+            getLogger().warning("No se encontró Vault o un plugin de economía. Los pagos con dinero estarán desactivados.");
+        }
+
+        this.bountyManager = new BountyManager(this);
+
+        getCommand("bounty").setExecutor(new BountyCommand(this));
+
+        getLogger().info("DayBounties habilitado correctamente.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("DayBounties deshabilitado.");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        this.economy = rsp.getProvider();
+        return true;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public BountyManager getBountyManager() {
+        return bountyManager;
+    }
+
+    public FileConfiguration getMessages() {
+        return getConfig();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+  }
